7c785096a7da44784fb8afa7016c06ec7e241b8f
Mackenzie Leng <kenz.joy@outlook.com>
2022-12-06 21:43:07 -0700

    adds a sad path for when there are no items

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -8,6 +8,38 @@ describe 'Items API' do
 
     expect(response).to be_successful
 
-    items = JSON.parse(response.body)
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items.count).to eq(20)
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(Integer)
+
+      expect(item).to have_key(:name)
+      expect(item[:name]).to be_a(String)
+
+      expect(item).to have_key(:description)
+      expect(item[:description]).to be_a(String)
+
+      expect(item).to have_key(:unit_price)
+      expect(item[:unit_price]).to be_a(Float)
+
+      expect(item).to have_key(:merchant_id)
+      expect(item[:merchant_id]).to be_an(Integer)
+    end
+  end
+
+  it 'sad path: returns an empty parse when there are no items' do
+    create_list(:item, 0)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items).to eq([])
+    expect(items.count).to eq(0)
   end
 end
