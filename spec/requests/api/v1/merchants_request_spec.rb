1e3c72068fca145e7f1b044f2739c1ad8d98f871
Mackenzie Leng <kenz.joy@outlook.com>
2022-12-06 17:12:45 -0700

    add sad path test for case where there are no merchants

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -21,6 +21,19 @@ describe 'Merchants API' do
     end
   end
 
+  it 'sad path: returns an empty parse when there are no merchants' do
+    create_list(:merchant, 0)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants).to eq([])
+    expect(merchants.count).to eq(0)
+  end
+
   it 'can get one merchant by its id' do
     id = create(:merchant).id
 
@@ -36,4 +49,6 @@ describe 'Merchants API' do
     expect(merchant).to have_key(:name)
     expect(merchant[:name]).to be_a(String)
   end
+
+  
 end
