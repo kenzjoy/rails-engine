5ba11f53106a2632493e3c99d9fa8fb2a272ddea
Mackenzie Leng <kenz.joy@outlook.com>
2022-12-08 15:34:16 -0700

    passing tests for find all items

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,6 @@ class Item < ApplicationRecord
   belongs_to :merchant
 
   def self.search_items(name)
-    require 'pry'; binding.pry
     where("name ILIKE ?", "%#{name}%")
   end
 end
--- spec/requests/api/items_search_spec.rb
+++ spec/requests/api/items_search_spec.rb
@@ -18,7 +18,39 @@ describe 'items search API' do
     search_parsed[:data].each do |item|
       expect(item[:id]).to be_a(String)
       expect(item[:type]).to eq("item")
-      expect(item[:attributes][:name].include?("color")).to eq(true)
+      expect(item[:attributes][:name].include?("Color")).to eq(true)
     end
+  end
+
+  it 'returns one item based on search criteria' do
+    item_1 = create(:item, name: 'Colorado Kolsch')
+    item_1 = create(:item, name: 'Colorado Kind Ale')
+    item_3 = create(:item, name: 'Backside Stout')
+    search = "color"
+
+    get "/api/v1/items/find?name=#{search}"
+
+    expect(response).to be_successful
+
+    search_parsed = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(search_parsed.length).to eq(1)
+    expect(search_parsed[:data]).to have_key(:id)
+    expect(search_parsed[:data][:id]).to eq("#{item_1.id}")
+    expect(search_parsed[:data][:id]).to_not eq("#{item_2.id}")
+    expect(search_parsed[:data][:attributes]).to have_key(:name)
+    expect(search_parsed[:data][:attributes][:name]).to eq('Colorado Kolsch')
+  end
+
+  it 'sad path: returns no items if there is not a query match' do
+    search = "banana"
+
+    get "/api/v1/items/find?name=#{search}"
+    
+    expect(response).to be_successful
+
+    search_parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(search_parsed[:data][:item]).to eq([])
   end
 end
