7c09bb402c7fbd8ff0b6131469363c32b97fdf8f
Mackenzie Leng <kenz.joy@outlook.com>
2022-12-07 22:33:42 -0700

    create model test for search_merchant class method

---
+++ app/controllers/api/v1/merchants_search_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::MerchantsSearchController < ApplicationController
+  def show
+    
+  end
+end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,5 +4,23 @@ RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it { should have_many(:items) }
   end
-  # pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'class methods' do
+    before(:each) do
+      @merchant_1 = create(:merchant, name: "Millionaire's Monopoly")
+      @merchant_2 = create(:merchant, name: "Skilled Sarah's Software")
+      @merchant_3 = create(:merchant, name: "aMARIllO's OAsis")
+      @merchant_4 = create(:merchant, name: "SpeakerBoxxx")
+      @merchant_5 = create(:merchant, name: "Aaron's Designs")
+    end
+
+    describe '#search_merchant' do
+      it 'returns one merchant based on search criteria' do
+        result = Merchant.search_merchant("iLl")
+
+        expect(result).to be_a(Merchant)
+        expect(result).to eq(@merchant_3)
+      end
+    end
+  end
 end
